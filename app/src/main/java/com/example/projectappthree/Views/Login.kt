791ee9b3abc648d7d04bcda55dc889b0776dedb5
Akash Choudhary <choudhary98akash@gmail.com>
2024-06-14 13:38:45 +0530

    added sigup page

--- app/src/main/java/com/example/projectappthree/Views/Login.kt
+++ app/src/main/java/com/example/projectappthree/Views/Login.kt
@@ -1,12 +1,10 @@
 package com.example.projectappthree.Views
 
-import android.util.Log
+import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -30,7 +28,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -45,9 +42,11 @@ fun Login(
     modifier: Modifier = Modifier
 ) {
     var email by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var LoginPage by remember { mutableStateOf(true) }
-    var contextForToast = LocalContext.current
+    var confirmPassword by remember { mutableStateOf("") }
+    var loginPage by remember { mutableStateOf(true) }
+    val contextForToast = LocalContext.current
     Column(
         horizontalAlignment = horizontalAlignment,
         verticalArrangement = verticalArrangement,
@@ -58,21 +57,22 @@ fun Login(
     ) {
 
 
-        if(LoginPage) {
-            Row( // Use Row for horizontal layout
+        if(loginPage) {
+            Row(
                 modifier = Modifier
-                    .fillMaxWidth(0.7f) // Adjust width as needed
-                    .padding(horizontal = 16.dp) // Add horizontal padding
+                    .fillMaxWidth(0.7f)
+                    .padding(horizontal = 16.dp)
                     .clip(RoundedCornerShape(30.dp))
                     .background(Color(245, 245, 245))
             ) {
 
                 Button(
-                    onClick = { LoginPage = true },
+                    onClick = {
+                    loginPage = true
+                    },
                     modifier = Modifier
                         .background(Color(34, 97, 229))
                         .weight((1F)),
-//                    .padding(horizontal = 16.dp, vertical = 8.dp),
                     shape = RoundedCornerShape(24.dp),
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color.Transparent
@@ -82,11 +82,10 @@ fun Login(
                 }
 
                 Button(
-                    onClick = { /* Handle login */ },
+                    onClick = { loginPage = false },
                     modifier = Modifier
                         .weight((1F))
                         .background(Color.Transparent),
-//                    .padding(horizontal = 16.dp, vertical = 8.dp),
                     shape = RoundedCornerShape(24.dp),
                     colors = ButtonDefaults.buttonColors(
                         containerColor = Color.Transparent
@@ -113,20 +112,19 @@ fun Login(
             TextField(
                 value = email,
                 onValueChange = { email = it },
-//            label = { Text("Email") },
                 modifier = Modifier
                     .fillMaxWidth(0.95f)
-                    .background(Color.White) // Set white background
+                    .background(Color.White)
                     .padding(horizontal = 16.dp, vertical = 8.dp)
                     .clip(RoundedCornerShape(30.dp)) // Rounded corners
                     .background(Color(245, 245, 245))
-                    .shadow( // Drop shadow
+                    .shadow(
                         elevation = 4.dp,
                         shape = RoundedCornerShape(20.dp),
                         ambientColor = Color(245, 245, 245),
                         spotColor = Color(245, 245, 245)
                     ),
-                colors = TextFieldDefaults.colors( // Remove underline
+                colors = TextFieldDefaults.colors(
                     focusedIndicatorColor = Color(245, 245, 245),
                     unfocusedIndicatorColor = Color(245, 245, 245)
                 )
@@ -146,13 +144,12 @@ fun Login(
             TextField(
                 value = password,
                 onValueChange = { password = it },
-//            label = { Text("Password") },
                 modifier = Modifier
                     .fillMaxWidth(0.95f)
-                    .background(Color.White) // Set white background
+                    .background(Color.White)
                     .padding(horizontal = 16.dp, vertical = 8.dp)
                     .clip(RoundedCornerShape(30.dp)) // Rounded corners
-                    .shadow( // Drop shadow
+                    .shadow(
                         elevation = 4.dp,
                         shape = RoundedCornerShape(20.dp),
                         ambientColor = Color.LightGray.copy(alpha = 0.2f),
@@ -197,12 +194,206 @@ fun Login(
             ) {
                 Text(text = "LOGIN", color = Color.White, fontSize = 20.sp)
             }
-        }else{
+        }
+        else{
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(0.7f) // Adjust width as needed
+                    .padding(horizontal = 16.dp) // Add horizontal padding
+                    .clip(RoundedCornerShape(30.dp))
+                    .background(Color(245, 245, 245))
+            ) {
 
+                Button(
+                    onClick = { loginPage = true },
+                    modifier = Modifier
+                        .background(Color.Transparent)
+                        .weight((1F)),
+//                    .padding(horizontal = 16.dp, vertical = 8.dp),
+                    shape = RoundedCornerShape(24.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent
+                    )
+                ) {
+                    Text(text = "LOGIN", color = Color.Black, fontSize = 16.sp)
+                }
+
+                Button(
+                    onClick = { loginPage = false},
+                    modifier = Modifier
+                        .weight((1F))
+                        .background(Color(34, 97, 229)),
+                    shape = RoundedCornerShape(24.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Transparent
+                    )
+                ) {
+                    Text(text = "SIGNUP", color = Color.White, fontSize = 16.sp)
+                }
+
+
+            }
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(0.7F)
+            ) {
+                Text(
+                    text = "Full Name",
+                    style = TextStyle(fontSize = 18.sp),
+                )
+
+            }
+
+            TextField(
+                value = username,
+                onValueChange = { username = it },
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .background(Color.White) // Set white background
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .clip(RoundedCornerShape(30.dp)) // Rounded corners
+                    .background(Color(245, 245, 245))
+                    .shadow( // Drop shadow
+                        elevation = 4.dp,
+                        shape = RoundedCornerShape(20.dp),
+                        ambientColor = Color(245, 245, 245),
+                        spotColor = Color(245, 245, 245)
+                    ),
+                colors = TextFieldDefaults.colors( // Remove underline
+                    focusedIndicatorColor = Color(245, 245, 245),
+                    unfocusedIndicatorColor = Color(245, 245, 245)
+                )
+            )
+
+
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(0.7F)
+            ) {
+                Text(
+                    text = "Email",
+                    style = TextStyle(fontSize = 18.sp),
+                )
+
+            }
+
+            TextField(
+                value = email,
+                onValueChange = { email = it },
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .background(Color.White) // Set white background
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .clip(RoundedCornerShape(30.dp)) // Rounded corners
+                    .background(Color(245, 245, 245))
+                    .shadow( // Drop shadow
+                        elevation = 4.dp,
+                        shape = RoundedCornerShape(20.dp),
+                        ambientColor = Color(245, 245, 245),
+                        spotColor = Color(245, 245, 245)
+                    ),
+                colors = TextFieldDefaults.colors( // Remove underline
+                    focusedIndicatorColor = Color(245, 245, 245),
+                    unfocusedIndicatorColor = Color(245, 245, 245)
+                )
+            )
+
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(0.7F)
+            ) {
+                Text(
+                    text = "Password",
+                    style = TextStyle(fontSize = 18.sp),
+                )
+
+            }
+            TextField(
+                value = password,
+                onValueChange = { password = it },
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .background(Color.White)
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .clip(RoundedCornerShape(30.dp)) // Rounded corners
+                    .shadow( // Drop shadow
+                        elevation = 4.dp,
+                        shape = RoundedCornerShape(20.dp),
+                        ambientColor = Color.LightGray.copy(alpha = 0.2f),
+                        spotColor = Color.LightGray.copy(alpha = 0.4f)
+                    ),
+                colors = TextFieldDefaults.colors( // Remove underline
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
+
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(0.7F)
+            ) {
+                Text(
+                    text = "Confirm Password",
+                    style = TextStyle(fontSize = 18.sp),
+                )
+
+            }
+            TextField(
+                value = confirmPassword,
+                onValueChange = { confirmPassword = it },
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .background(Color.White)
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .clip(RoundedCornerShape(30.dp)) // Rounded corners
+                    .shadow( // Drop shadow
+                        elevation = 4.dp,
+                        shape = RoundedCornerShape(20.dp),
+                        ambientColor = Color.LightGray.copy(alpha = 0.2f),
+                        spotColor = Color.LightGray.copy(alpha = 0.4f)
+                    ),
+                colors = TextFieldDefaults.colors( // Remove underline
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent
+                )
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+
+                onClick = {
+                    Toast.makeText(
+                        contextForToast,
+                        "Signup Button has been Clicked.",
+                        Toast.LENGTH_LONG
+                    ).show()
+                },
+                modifier = Modifier
+                    .fillMaxWidth(0.95f)
+                    .fillMaxHeight(0.138f)
+//                    .padding(horizontal = 16.dp, vertical = 8.dp)
+                    .shadow(20.dp, RoundedCornerShape(24.dp)), // Add shadow
+                shape = RoundedCornerShape(24.dp), // Rounded corners
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(34, 97, 229)
+                )
+            ) {
+                Text(text = "SIGNUP", color = Color.White, fontSize = 20.sp)
+            }
         }
 
     }
 }
+
+
 
 @Preview
 @Composable
